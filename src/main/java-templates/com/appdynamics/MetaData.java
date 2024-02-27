7eefbbe13c70abd00ea2c9adb65ac2d25e0a88cc
John Southerland <jsoutherland@me.com>
2024-02-27 08:57:59 -0700

    changed support email to my own

--- src/main/java-templates/com/appdynamics/MetaData.java
+++ src/main/java-templates/com/appdynamics/MetaData.java
@@ -3,5 +3,5 @@ package com.appdynamics;
 public class MetaData {
     public static final String VERSION = "v${plugin-version}";
     public static final String BUILDTIMESTAMP = "${timestamp}";
-    public static final String CONTACT_GECOS = "support@appdynamics.com";
+    public static final String CONTACT_GECOS = "john.southerland@appdynamics.com";
 }
